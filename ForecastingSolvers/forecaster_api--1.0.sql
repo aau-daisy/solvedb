509293df8b35c52a7ae155553e157f0673be889b
Davide <david.frazzetto@gmail.com>
2017-05-31 18:26:37 +0300

    updates on predictive solver added forecaster api

---
+++ ForecastingSolvers/forecaster_api--1.0.sql
@@ -0,0 +1,114 @@
+﻿-- *********UTILITY FUNCTIONS FOR FORECASTING SOLVERS *********--
+
+-- Converts a data string from an unknown date format to date of format YYYY-MM-DD HH:MM:SS
+drop function if exists convert_date_string(text);
+CREATE OR REPLACE FUNCTION convert_date_string(date_string text) RETURNS text
+AS $$
+	import dateutil.parser as parser
+	try:
+		return parser.parse(date_string)
+	except Exception:
+		return None
+$$ LANGUAGE plpythonu;
+
+-- CREATE FUNCTION separate_input_relation_on_time_range(target_column_name text,
+-- 	final_ts_features text[], timeFrequency int, input_table_tmp_name text) RETURNS text
+-- AS $$
+-- 
+-- 	from datetime import datetime
+-- 	from datetime import timedelta
+-- 	 
+-- 	starting_datetime = datetime.strptime(starting_time, '%Y-%m-%d %H:%M:%S')
+-- 	ending_datetime = datetime.strptime(ending_time, '%Y-%m-%d %H:%M:%S')
+-- 
+-- 	# TODO check if time range is correct: end > start
+-- 	# find most probable interval between samples in time series
+-- 	query = "select " + time_column_name + " from " + table_name + " order by " + time_column_name + " desc limit 1000"
+-- 	rv = plpy.execute(query)
+-- 	time_intervals = {}
+-- 	for i in range(len(rv)-1):
+-- 		time_object_a = datetime.strptime(rv[i][time_column_name], '%Y-%m-%d %H:%M:%S')
+-- 		time_object_b = datetime.strptime(rv[i+1][time_column_name], '%Y-%m-%d %H:%M:%S')
+-- 		print (time_object_a - time_object_b).total_seconds()
+-- 		time_intervals[(time_object_a - time_object_b).total_seconds()] = time_intervals.get((time_object_a - time_object_b).total_seconds(),0) + 1
+-- 	most_probable_interval = None
+-- 	probability = 0
+-- 	for key, value in time_intervals.iteritems():
+-- 		if value > probability:
+-- 			probability = value
+-- 			most_probable_interval = key
+-- 
+-- 	# create temporary table with rows to fill
+-- 	query = "select " + time_column_name + ", " + target_column_name + " from " + table_name + " where " + time_column_name + " > \'" + str(starting_datetime) + "\' and " + time_column_name + " < \'" + str(ending_datetime) + "\'"
+-- 	rv = plpy.execute(query)
+-- 	lines_for_view = []
+-- 	last_existing_line = starting_datetime
+-- 	for line in rv:
+-- 		last_existing_line = datetime.strptime(line[time_column_name], '%Y-%m-%d %H:%M:%S')
+-- 		# if the value column is empty, mark as to_be_filled
+-- 		if not line[target_column_name]:
+-- 			lines_for_view.append({'time':line[time_column_name], 'value':'null','fill':True})
+-- 		else:
+-- 			lines_for_view.append({'time':line[time_column_name], 'value':line[target_column_name], 'fill':False})
+-- 
+-- 
+-- 	number_of_rows_to_fill = int((ending_datetime - last_existing_line).total_seconds() / float(most_probable_interval))
+-- 	for i in range(1, number_of_rows_to_fill+1):
+-- 		lines_for_view.append({'time':str(last_existing_line + timedelta(seconds=i * 3600)), 'value':'null', 'fill':True})
+-- 
+-- 	# write the temporary table on the db
+-- 	tmp_table_name = "tmp_forecasting_table_" + table_name
+-- 	query = "drop table if exists " + tmp_table_name + " cascade"
+-- 	plpy.execute(query)
+-- 	query = "create table " + tmp_table_name + " (" + time_column_name  + " TIMESTAMP, " + target_column_name +   " NUMERIC, fill BOOLEAN)"
+-- 	plpy.execute(query)
+-- 
+-- 	query = "INSERT INTO " + tmp_table_name + " VALUES "
+-- 	for data in lines_for_view:
+-- 		query += "('{time}', {value}, {fill}),\n".format(**data)
+-- 	print query
+-- 	plpy.execute(query[:-2])
+-- 
+-- 	return tmp_table_name;
+-- 
+-- 
+-- 
+-- 
+-- 
+-- 
+-- $$ LANGUAGE plpythonu;
+
+
+
+drop function if exists separate_input_relation_on_empty_rows(text, text[], text);
+CREATE OR REPLACE FUNCTION separate_input_relation_on_empty_rows(target_column_name text, 
+		feature_column_names text[], table_name text)
+  RETURNS name
+AS $$
+DECLARE
+	target_table_name text := sl_get_unique_tblname() || '_ml_target_view';
+	query	text := 'SELECT COUNT(*) FROM ';
+	c 	int;
+BEGIN
+	EXECUTE format('CREATE TEMP VIEW %s (%s, %s) AS SELECT %s, %s FROM %s WHERE %s is null',
+			target_table_name,
+			(SELECT string_agg(format('%s',quote_ident(feature_column_names[j])), ',')
+				FROM generate_subscripts(feature_column_names, 1) AS j),
+			target_column_name,
+			(SELECT string_agg(format('%s',quote_ident(feature_column_names[j])), ',')
+				FROM generate_subscripts(feature_column_names, 1) AS j),
+			target_column_name,
+			table_name,
+			target_column_name);
+	query := query || target_table_name;     
+	execute query into c ;
+	-- check that the table contains data to fill
+	IF c = 0 THEN
+		RETURN null;
+	ELSE
+		RETURN target_table_name;
+	END IF;
+END
+
+$$ LANGUAGE plpgsql STRICT;
+------------------------------------------------------------------------------------------------------------------------------------------
--- ForecastingSolvers/forecasting_advisor_solver/predictive_solver--1.0.sql
+++ ForecastingSolvers/forecasting_advisor_solver/predictive_solver--1.0.sql
@@ -36,22 +36,47 @@ DECLARE
 	input_length		int;
 	ts_features		text[] := '{}';
 	ml_features		text[] := '{}';
-	
+	timeFrequency		int;
+	ml_target_view		text;
+	ts_target_view		text;
      
 BEGIN       
 
 	raise notice 'this is the default method';   
 	PERFORM sl_create_view(sl_build_out(arg), input_table_tmp_name); 
-	
+
+
+	--------//////     	SETUP		////-------------------------------
 	
      -- Check if the arguments are given and table format are correct
+	IF array_length(((arg).problem).cols_unknown, 1) != 1 THEN
+		RAISE EXCEPTION 'Single target prediction supported. 
+		Select a single column as SOLVESELECT target, 
+		e.g. "SOLVESELECT your_column IN (SELECT * FROM your_table) USING predictive_solver()"';
+	END IF;
 	target_column_name 	= ((arg).problem).cols_unknown[1];
 	attMethods       	= sl_param_get_as_text(arg, 'methods');
 	attFeatures       	= sl_param_get_as_text(arg, 'features');
 	attStartTime       	= sl_param_get_as_text(arg, 'start_time');
 	attEndTime       	= sl_param_get_as_text(arg, 'end_time');
 	attFrequency       	= sl_param_get_as_text(arg, 'frequency');
-
+	
+	-- check the ensamble of forecasting methods to test (from input or default)
+	IF attMethods is null THEN
+		ml_methods_to_test := enum_range(null::sl_supported_ml_forecasting_methods)::text[];
+		ts_methods_to_test := enum_range(null::sl_supported_ts_forecasting_methods)::text[];
+	ELSE
+		tmp_string_array := string_to_array(attMethods, ',');
+		for i in 1..array_length(tmp_string_array,1) LOOP
+			IF coalesce((select tmp_string_array[i] = any (enum_range(null::sl_supported_ml_forecasting_methods)::text[])), false) then
+				ml_methods_to_test := ml_methods_to_test || tmp_string_array[i];
+			ELSIF coalesce((select tmp_string_array[i] = any (enum_range(null::sl_supported_ts_forecasting_methods)::text[])), false) then
+				ts_methods_to_test := ts_methods_to_test || tmp_string_array[i];
+			ELSE
+				raise exception 'The method "%" is not supported. The currently supported methods are: %, %', tmp_string_array[i], enum_range(null::sl_supported_ml_forecasting_methods)::text[], enum_range(null::sl_supported_ts_forecasting_methods)::text[];
+			END IF;
+		END LOOP;
+	END IF;
 
 	-- separates columns in target, time columns and feature columns
 	for input_clmn in select att_name, att_type from  sl_get_attributes_from_sql('select * from pr_input_relation limit 1')
@@ -69,10 +94,76 @@ BEGIN
 		end if;		
 	 end loop;
 
+	-- check time arguments (if time columns are present in the table)
+	 if (attStartTime IS NOT NULL OR attEndTime IS NOT NULL) AND array_length(input_time_col_names,1) = 0 THEN
+		RAISE EXCEPTION 'No time columns present in the given Table. Impossible to process arguments.';
+	 END IF;
 
-	-- create K views for k cross folding
+	IF attStartTime IS NOT NULL THEN
+		attStartTime = convert_date_string(attStartTime);
+		IF attStartTime IS NULL THEN
+			RAISE EXCEPTION 'Given START TIME is not a recognizable time format, or the date is incorrect.';
+		END IF;
+		raise notice 'time %', attStartTime;
+	END IF;
+	IF attEndTime IS NOT NULL THEN
+		attEndTime = convert_date_string(attEndTime);
+		IF attEndTime IS NULL THEN
+			RAISE EXCEPTION 'Given END TIME is not a recognizable time format, or the date is incorrect.';
+		END IF;
+	END IF;
+	IF attFrequency IS NOT NULL THEN
+		timeFrequency = attFrequency::int; 
+	END IF;
+
+	IF (attStartTime IS NOT NULL AND attEndTime IS NULL) OR (attStartTime IS NULL AND attEndTime IS NOT NULL) THEN
+		RAISE EXCEPTION 'Time interval needs to be defined with <start,end> as 
+			start_time:="your_start_time", end_time:="your_end_time"';
+	END IF;
+
+	 -- set the user defined features (if found)
+	IF attFeatures IS NULL THEN
+		-- NOTHING, THE FEATURE SELECTOR WILL CHOOSE THE FEATURES
+		final_ts_features := input_time_col_names;
+		final_ml_features := input_feature_col_names;
+	ELSE
+		tmp_string_array := string_to_array(attFeatures, ',');
+		for i in 1..array_length(tmp_string_array,1) LOOP
+			IF (select tmp_string_array[i] = ANY (input_feature_col_names::text[])) then
+				final_ml_features := final_ml_features || tmp_string_array[i];
+			ELSIF (select tmp_string_array[i] = ANY (input_time_col_names::text[])) then
+ 				final_ts_features := final_ts_features || tmp_string_array[i];
+ 			ELSE
+				RAISE EXCEPTION 'Column % not present in input Table', tmp_string_array[i];
+			END IF;
+		END LOOP;
+		-- check if multiple time columns are present, alert the user
+		if array_length(ts_methods_to_test,1) > 1 AND array_length(final_ts_features, 1) > 1 then
+			raise notice 'Multiple time variables in input schema. Column % will be used. To select a specific column for the time series, add argument "time_column := name_of_time_column"', final_ts_features[1];
+		end if;
+	END IF;
+	RAISE NOTICE '%, %', final_ml_features, final_ts_features[1];
+
+
+	 -- split into training data (with filled target column) and target data (with empty target column)
+	 -- if time range is given, and a time column is present, prepare the time series table with additional rows with for given range
+	 -- if time range is not given AND no empty rows are found
+		-->test on the training set and give the results of the prediction to the user
+
+
+	
+
+	ml_target_view := separate_input_relation_on_empty_rows(target_column_name, final_ml_features, input_table_tmp_name);
+	raise notice 'table is % of rows mooom', ml_target_view;
+
+	-- ts_target_view := separate_input_relation_on_time_range(target_column_name, final_ts_features[1], 
+-- 		attStartTime, attEndTime, timeFrequency, input_table_tmp_name)
+	-->RESULT: TRAINING_VIEW, TEST_VIEW
+
+
+	-- create K views for k cross folding on the training data
 	if array_length(input_feature_col_names, 1) > 0 THEN
-		SELECT COUNT(*) INTO input_length FROM pr_input_relation;
+		execute 'SELECT COUNT(*) FROM $1' into input_length using pr_input_relation;
 		for i in 0..(k-1) loop
 			EXECUTE format('CREATE OR REPLACE TEMP VIEW pr_cross_test_%s (%s,%s) AS SELECT %s,%s FROM %s LIMIT (%s) OFFSET (%s)', 
 				i,
@@ -102,62 +193,23 @@ BEGIN
 
 	-- Create 70%-30% split for time series forecasting
 	IF array_length(input_time_col_names,1) > 0 THEN
-		EXECUTE format('CREATE OR REPLACE TEMP VIEW % AS SELECT %s,%s FROM %s LIMIT %s',
-			ts_training_view
+		EXECUTE format('CREATE OR REPLACE TEMP VIEW %s AS SELECT %s,%s FROM %s LIMIT %s',
+			ts_training_view,
 			input_time_col_names[0],
 			target_column_name,
 			input_table_tmp_name,
 			((input_length/100) * 70)::int);
-		EXECUTE format('CREATE OR REPLACE TEMP VIEW % AS SELECT %s,%s FROM %s OFFSET %s',
-			ts_test_view
+		EXECUTE format('CREATE OR REPLACE TEMP VIEW %s AS SELECT %s,%s FROM %s OFFSET %s',
+			ts_test_view,
 			input_time_col_names[0],
 			target_column_name,
 			input_table_tmp_name,
 			((input_length/100) * 70)::int);
 	END IF;
-
-	-- set the ensamble of forecasting methods to test
-	IF attMethods is null THEN
-		ml_methods_to_test := enum_range(null::sl_supported_ml_forecasting_methods)::text[];
-		ts_methods_to_test := enum_range(null::sl_supported_ts_forecasting_methods)::text[];
-	ELSE
-		tmp_string_array := string_to_array(attMethods, ',');
-		for i in 1..array_length(tmp_string_array,1) LOOP
-			IF coalesce((select tmp_string_array[i] = any (enum_range(null::sl_supported_ml_forecasting_methods)::text[])), false) then
-				ml_methods_to_test := ml_methods_to_test || tmp_string_array[i];
-			ELSIF coalesce((select tmp_string_array[i] = any (enum_range(null::sl_supported_ts_forecasting_methods)::text[])), false) then
-				ts_methods_to_test := ts_methods_to_test || tmp_string_array[i];
-			ELSE
-				raise exception 'The method "%" is not supported. The currently supported methods are: %, %', tmp_string_array[i], enum_range(null::sl_supported_ml_forecasting_methods)::text[], enum_range(null::sl_supported_ts_forecasting_methods)::text[];
-			END IF;
-		END LOOP;
-	END IF;
-
-
-
-	-- set the user defined features (if found)
-	IF attFeatures IS NULL THEN
-		-- NOTHING, THE FEATURE SELECTOR WILL CHOOSE THE FEATURES
-		null;
-	ELSE
-		tmp_string_array := string_to_array(attFeatures, ',');
-		for i in 1..array_length(tmp_string_array,1) LOOP
-			IF (select tmp_string_array[i] = ANY (input_feature_col_names::text[])) then
-				final_ml_features := final_ml_features || tmp_string_array[i];
-			ELSIF (select tmp_string_array[i] = ANY (input_time_col_names::text[])) then
- 				final_ts_features := final_ts_features || tmp_string_array[i];
- 			ELSE
-				RAISE EXCEPTION 'Column % not present in input Table', tmp_string_array[i];
-			END IF;
-		END LOOP;
-		-- check if multiple time columns are present, alert the user
-		if array_length(final_ts_features, 1) > 1 then
-			raise notice 'Multiple time variables in input schema. Column % will be used. To select a specific column for the time series, add argument "time_column := name_of_time_column"', input_time_col_names[1];
-		end if;
-	END IF;
-	RAISE NOTICE '%, %', final_ml_features, final_ts_features;
 
 	
+
+
 	-- set the features for the models, if given by the user
 	
 
@@ -191,13 +243,20 @@ BEGIN
 -- 
 --          RETURN QUERY EXECUTE sl_return(arg, sl_build_union_right_join_debug(arg, test_not_joining_columns, test_joining_columns, tmp_forecasting_table_name));
          perform sl_drop_view_cascade(input_table_tmp_name);
-     
+
+
+	EXCEPTION
+		-- conversion of the method's arguments to integer failed 
+		WHEN SQLSTATE '22P02' THEN
+			RAISE EXCEPTION 'Impossible to parse given argument/s';
 
 END;
-$$ LANGUAGE plpgsql VOLATILE STRICT;
+$$ LANGUAGE plpgsql STRICT;
 
 
 -- 
 solveselect watt in (select * from Test) 
-using predictive_solver(features:='time_t');
+using predictive_solver();
+
+
 
